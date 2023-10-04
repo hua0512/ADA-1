a562b7945cb5ccf2f0075c779e4d0992b8df43c2
hua0512 <whua0512@gmail.com>
2023-10-04 10:48:00 +0200

    Corregida parte 1

--- src/Programa1.java
+++ src/Programa1.java
@@ -31,76 +31,114 @@ public class Programa1 extends BaseProgram {
 
 
     // Cabezera del fichero
-    String prefixBuilder = "K size, " +
+    String prefixBuilder = "Vector size, " +
             "Comparisons, " +
             "Assignments," +
             "Media," +
             "Time(ns)\n";
     StringBuilder resultStringBuilder = new StringBuilder(prefixBuilder);
+    StringBuilder resultStringBuilder2 = new StringBuilder(prefixBuilder);
+    StringBuilder resultStringBuilder3 = new StringBuilder(prefixBuilder);
 
 
     /**
      * Tamaño del vector
      */
-    int TAM = 100000;
+    int TAM = 10000;
 
     // vector de enteros
     int[] vector;
 
-    // indice k
-    int k = MIN_K;
+    // alg1
+    DataConfig data1 = new DataConfig();
+    // alg 2
+    DataConfig data2 = new DataConfig();
+//    alg 3
+    DataConfig data3 = new DataConfig();
 
-    // dato quicksort modificado
-    DataConfig data = new DataConfig();
+    while (TAM <= 100000) {
 
-    for (int i = 0; i < 2; i++) {
+      vector = new int[TAM];
 
-      // maximo k = 30
-      while (k <= MAX_K) {
+      for (int i = 0; i < REPETICIONES; i++) {
+        // relleno del vector
+        Algorithms.populate(vector);
+        // descolacion de elementos
+        Algorithms.randomize(vector, vector.length);
 
-        vector = new int[TAM];
+        Instant startTime = Instant.now();
+        Algorithms.ordena1(vector, vector.length, data1);
+        data1.incrementarTime(Duration.between(startTime, Instant.now()).toNanos());
 
+        // relleno del vector
+        Algorithms.populate(vector);
+        // descolacion de elementos
+        Algorithms.randomize(vector, vector.length);
 
-        // 20 repeticiones para cada k
-        for (int z = 0; z < REPETICIONES; z++) {
-          System.out.println("Starting TAM : " + i + " k = "  + k );
-          // relleno del vector
-          Algorithms.populate(vector);
-          // descolacion de elementos
-          Algorithms.randomize(vector, vector.length);
+        startTime = Instant.now();
+        Algorithms.ordena2(vector, vector.length, data2);
+        data2.incrementarTime(Duration.between(startTime, Instant.now()).toNanos());
 
-          Instant startTime = Instant.now();
-          if (z == 0) {
-            Algorithms.ordena1(vector, vector.length, data);
-          } else if (z == 1) Algorithms.ordena2(vector, vector.length, data);
-          else Algorithms.ordena3(vector, vector.length, data);
-          data.incrementarTime(Duration.between(startTime, Instant.now()).toNanos());
-        }
+        // relleno del vector
+        Algorithms.populate(vector);
+        // descolacion de elementos
+        Algorithms.randomize(vector, vector.length);
 
-
-        long mediaComps = data.getComps() / REPETICIONES;
-        long mediaAsigs = data.getAsigs() / REPETICIONES;
-        long media = (mediaComps + mediaAsigs) / 2;
-        long mediaTime = data.getTime() / REPETICIONES;
-
-        resultStringBuilder.append(k).append(',')
-                .append(mediaComps).append(',')
-                .append(mediaAsigs).append(',')
-                .append(media).append(',')
-                .append(mediaTime)
-                .append("\n");
-
-        data.setAsigs(0);
-        data.setComps(0);
-        data.setTime(0);
-        k++;
+        startTime = Instant.now();
+        Algorithms.ordena3(vector, vector.length, data3);
+        data3.incrementarTime(Duration.between(startTime, Instant.now()).toNanos());
       }
 
+      long mediaComps1 = data1.getComps() / REPETICIONES;
+      long mediaAsigs1 = data1.getAsigs() / REPETICIONES;
+      long media1 = (mediaComps1 + mediaAsigs1) / 2;
+      long mediaTime1 = data1.getTime() / REPETICIONES;
 
+      long mediaComps2 = data2.getComps() / REPETICIONES;
+      long mediaAsigs2 = data2.getAsigs() / REPETICIONES;
+      long media2 = (mediaComps2 + mediaAsigs2) / 2;
+      long mediaTime2 = data2.getTime() / REPETICIONES;
+
+      long mediaComps3 = data3.getComps() / REPETICIONES;
+      long mediaAsigs3 = data3.getAsigs() / REPETICIONES;
+      long media3 = (mediaComps3 + mediaAsigs3) / 2;
+      long mediaTime3 = data3.getTime() / REPETICIONES;
+
+      resultStringBuilder.append(TAM).append(',')
+              .append(mediaComps1).append(',')
+              .append(mediaAsigs1).append(',')
+              .append(media1).append(',')
+              .append(mediaTime1)
+              .append("\n");
+
+      resultStringBuilder2.append(TAM).append(',')
+              .append(mediaComps2).append(',')
+              .append(mediaAsigs2).append(',')
+              .append(media2).append(',')
+              .append(mediaTime2)
+              .append("\n");
+
+      resultStringBuilder3.append(TAM).append(',')
+              .append(mediaComps3).append(',')
+              .append(mediaAsigs3).append(',')
+              .append(media3).append(',')
+              .append(mediaTime3)
+              .append("\n");
+
+      data1.setAsigs(0);
+      data1.setComps(0);
+      data1.setTime(0);
+      data2.setAsigs(0);
+      data2.setComps(0);
+      data2.setTime(0);
+      data3.setAsigs(0);
+      data3.setComps(0);
+      data3.setTime(0);
+
+      TAM += 10000;
     }
 
-
     // escribimos los resultados
-    outputResults(resultFile, resultStringBuilder.toString());
+    outputResults(resultFile, resultStringBuilder + "\n" + resultStringBuilder2 + "\n" + resultStringBuilder3);
   }
 }
